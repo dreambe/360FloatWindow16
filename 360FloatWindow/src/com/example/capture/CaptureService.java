0ee96c673eeb40a73077a01cd1e8262830e55091
zhouzijiang <zhouzijiang88@126.com>
2014-07-26 11:51:02 +0800

    截屏分享调整代码结构

--- 360FloatWindow/src/com/example/capture/CaptureService.java
+++ 360FloatWindow/src/com/example/capture/CaptureService.java
@@ -13,17 +13,16 @@ import pkg.screenshot.ScreenshotPolicy.OnAcquireRootPermissionCallback;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.os.Bundle;
 import android.os.Environment;
 import android.os.IBinder;
-import android.os.Message;
 import android.util.Log;
 
 public class CaptureService extends Service implements OnAcquireRootPermissionCallback{
 	
-	static final String BASE_PATH = Environment.getExternalStorageDirectory() + "/screens_shot";
-    public static String imageFile = "";
+	public static final String BASE_PATH = Environment.getExternalStorageDirectory() + "/screens_shot";
+	public static String imageFile = "";
 	File mFile;
+	
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
@@ -32,16 +31,13 @@ public class CaptureService extends Service implements OnAcquireRootPermissionCa
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		
-//		  Intent inten2t = new Intent();
-//		  inten2t.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK); 
-//		  inten2t.setClass(this,CapturedImageActivity.class);
-//		  startActivity(inten2t);
-//		  return super.onStartCommand(intent, flags, startId);
-		
-		if (ScreenshotPolicy.isCurrentAppAcquiredRoot()) {
+		if (ScreenshotPolicy.isCurrentAppAcquiredRoot()) 
+		{
 			Log.e("root", "already have root");
 			new CaptureThread().start();
-		}else{
+		}
+		else
+		{
 			ScreenshotPolicy.acquireRootPermission(CaptureService.this);
 		}
 		
@@ -63,59 +59,60 @@ public class CaptureService extends Service implements OnAcquireRootPermissionCa
 
     }
 	
+    private String getPath() 
+    {
+        if (mFile == null) {
+            mFile = new File(BASE_PATH);
+        }
+        if (!mFile.exists()) {
+            mFile.mkdirs();
+        }
+        imageFile = imageFile + "/" + System.currentTimeMillis() + ".jpg";
+        return imageFile;
+    }
 	
 	
-    class CaptureThread extends Thread{
-    	
+    class CaptureThread extends Thread
+    {	
     	@Override
-		public void run(){   		 
+		public void run()
+    	{   		 
     		
     		  Screenshot screenshot = Screenshot.getInstance();
 
               int result = screenshot.takeScreenshot();
               Log.i("CapureThread", "screen shot result:" + result);
-              if (result == Screenshot.SCREENSHOT_OK || result == Screenshot.SCREENSHOT_ARE_SAME) {
-                  try {
+              if (result == Screenshot.SCREENSHOT_OK || result == Screenshot.SCREENSHOT_ARE_SAME) 
+              {
+                  try 
+                  {
                 	  Bitmap map = screenshot.generateScreenshotBitmap();
                       String path = getPath();
-                      if (map != null) {
+                      if (map != null)
+                      {
                           ScreenshotUtils.saveBitmap2File(map, path);
-                          Log.i("sssss", "saveBitmap2File path[" + path + "] OK");
+                          Log.i("CapureThread", "saveBitmap2File path[" + path + "] OK");
                     	  Intent intent = new Intent();
                     	  Bitmap map2 = Bitmap.createBitmap(map); 
                     	  CapturedImageActivity.setBitmap(map2);
     					  intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK); 
     					  intent.setClass(CaptureService.this,CapturedImageActivity.class);
     					  startActivity(intent);
-                          // map.recycle();
-                          map = null;
                       }
                 	  
-                	  
-
-                  } catch (OutOfMemoryError e) {
+                  } 
+                  catch (OutOfMemoryError e) 
+                  {
                       Log.e("CapureThread", "Screenshot Thread generate Screenshot error:", e);
-                  } catch (IOException e) {
-                  Log.e("IOException", "EXCEPTION:", e);
-              }
+                  } 
+                  catch (IOException e) 
+                  {
+                	  Log.e("IOException", "EXCEPTION:", e);
+                  }
               }
               screenshot.release();
     	}
     	
-    	
-        private String getPath() {
-            if (mFile == null) {
-                mFile = new File(BASE_PATH);
-            }
-            if (!mFile.exists()) {
-                mFile.mkdirs();
-            }
-            // get system time
-            imageFile = BASE_PATH + "/" + System.currentTimeMillis() + ".jpg";
-            return imageFile;
-        }
-    	
-
     }
 
 }
